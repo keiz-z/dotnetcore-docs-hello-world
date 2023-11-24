0271114655e3e210067436c8dc171a5d33398cd7
keiz-z <151887019+keiz-z@users.noreply.github.com>
2023-11-24 18:24:53 +0900

    cs追加

---
+++ Pages/PaymentPageTest.cshtml.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YourAppNamespace
+{
+    public class PaymentPageTestModel : PageModel
+    {
+        // ここにページに関連するプロパティやメソッドを定義します。
+
+        // ページがロードされたときに呼ばれるメソッド
+        public void OnGet()
+        {
+            // ここにページが最初にリクエストされたときのロジックを書きます。
+        }
+
+        // 支払いが行われたときに呼ばれるメソッド
+        public IActionResult OnPostProcessPayment()
+        {
+            // ここに支払い処理のロジックを書きます。
+
+            // 支払い処理後にリダイレクトするページを指定
+            return RedirectToPage("PaymentSuccess");
+        }
+    }
+}
